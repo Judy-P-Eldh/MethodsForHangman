d516e0993a812a8589f7c6d3075a0b9f40ef79df
Judy-P-Eldh <pernilla.eldh@gmail.com>
2022-05-25 15:05:41 +0200

    Add project files.

---
+++ MethodsForHangman/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int count = 10;
+
+static int CountDown(int count)
+{
+    for (int i = 10; i > 0; i--)
+    {
+        Console.WriteLine(i.ToString());
+        Console.WriteLine(count);
+    }
+    return count;
+    
+}
+
