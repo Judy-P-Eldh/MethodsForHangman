8e872a3d282ee4c2bcda75eafe826c16217bae18
Judy-P-Eldh <pernilla.eldh@gmail.com>
2022-05-27 15:37:16 +0200

    Added Validations

---
+++ MethodsForHangman/Validations.cs
@@ -0,0 +1,18 @@
+﻿namespace MethodsForHangman
+{
+    public static class Validations
+    {
+        public static bool ValidateInput(string input)
+        {
+            if (input.All(c => char.IsDigit(c)))
+            {
+                return false;
+            }
+            else if (!string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
